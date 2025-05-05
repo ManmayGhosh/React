f211ed68fd046fad09ef0e0fe25cc7c110b0ee37
MANMAY GHOSH <manmayghosh01609@gmail.com>
2025-05-06 02:13:00 +0530

    phase1

---
+++ 07Currency/components/InputBox.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectedCurrency = 'USD',
+    amountDisabled = false,
+    currencyDisabled = false,
+    className = ""
+}) {
+
+    return (
+        <div
+            className={`bg-white p-3 rounded-lg text-sm flex ${className}`}
+        >
+            <div 
+            className="w-1-2"
+            >
+                <label 
+                className="text-black/40 mb-2 inline-block"
+                >
+                    {label}
+                </label>
+            </div>
+        </div>
+    )
+}
+
+export default InputBox
