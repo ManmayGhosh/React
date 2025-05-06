5021dfc8821b2d04a384649316730318c0aae147
MANMAY GHOSH <manmayghosh01609@gmail.com>
2025-05-06 17:13:29 +0530

    flex

--- 07Currency/components/InputBox.jsx
+++ 07Currency/components/InputBox.jsx
@@ -16,14 +16,26 @@ function InputBox({
         <div
             className={`bg-white p-3 rounded-lg text-sm flex ${className}`}
         >
-            <div 
-            className="w-1-2"
+            <div
+                className="w-1-2"
             >
-                <label 
-                className="text-black/40 mb-2 inline-block"
+                <label
+                    className="text-black/40 mb-2 inline-block"
                 >
                     {label}
                 </label>
+                <input
+                    type="number"
+                    className="outline-none w-full bg-transparent py-1.5"
+                    placeholder="AMOUNT"
+                    disabled={amountDisabled}
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                />
+            </div>
+            <div
+                className="w-1/2 flex flex-wrap justify-end text-right"
+            >
             </div>
         </div>
     )
