b09fd8939392c68715332e33a6987d4f642c699f
MANMAY GHOSH <manmayghosh01609@gmail.com>
2025-06-15 15:15:25 +0530

    5version

--- 09reactRouter/src/main.jsx
+++ 09reactRouter/src/main.jsx
@@ -3,18 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Layout from './Layout.jsx'
-import { Route ,createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import { Route ,RouterProvider,createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element = {<Layout></Layout>}>
-
+    <Route path='/' element = {<Layout />}>
+      <Route path = '' element={<Home />} />
+      <Route path = 'about' element={<About />} />
     </Route>
   )
 )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
