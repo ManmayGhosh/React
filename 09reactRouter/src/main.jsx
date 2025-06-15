f15d2d6e10555348039c67437aec5ea0df4d4a09
MANMAY GHOSH <manmayghosh01609@gmail.com>
2025-06-15 14:59:19 +0530

    next phase

--- 09reactRouter/src/main.jsx
+++ 09reactRouter/src/main.jsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Layout from './Layout.jsx'
+import { Route ,createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element = {<Layout></Layout>}>
+
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
